61c225e766b427fa71541488de9668f217bb6f51
unknown <sharifulislamshihan@gmail.com>
2024-02-16 16:58:12 +0600

    cart collection and cart item set in the database

--- index.js
+++ index.js
@@ -29,12 +29,36 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const menuCollection = client.db("tksBistroDb").collection("menu");
+        const cartCollection = client.db("tksBistroDb").collection("carts");
+
+        // getting info about menu (find)
+        app.get('/menu', async(req, res) => {
+            const result = await menuCollection.find().toArray();
+            res.send(result);
+        })
+
+
+        // carts Collection
+        app.get('/carts', async (req, res) =>{
+            const result = await cartCollection.find().toArray();
+            res.send(result)
+        })
+        // insertion
+        app.post('/carts', async(req, res) =>{
+            const cartItem = req.body;
+            const result = await cartCollection.insertOne(cartItem)
+            res.send(result);
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
