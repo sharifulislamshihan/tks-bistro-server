bac734cec3a0c5513c0c2d3a98c5459940a83879
unknown <sharifulislamshihan@gmail.com>
2024-02-20 00:00:35 +0600

    admin role checked

--- index.js
+++ index.js
@@ -42,19 +42,19 @@ async function run() {
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,
                 { expiresIn: '1h' });
 
-                res.send({ token });
+            res.send({ token });
         })
 
         // middleware to verify token
-        const verifyToken = (req, res, next) =>{
+        const verifyToken = (req, res, next) => {
             console.log('inside verify token', req.headers.authorization);
-            if(!req.headers.authorization){
-                return res.status(401).send({message: 'forbidden access'})
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'forbidden access' })
             }
-            const token  = req.headers.authorization.split(' ')[1];
-            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
-                if(err){
-                    return res.status(401).send({message: 'forbidden access'})
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'forbidden access' })
                 }
                 req.decoded = decoded;
                 next();
@@ -72,6 +72,22 @@ async function run() {
             console.log(req.headers);
             const result = await userCollection.find().toArray();
             res.send(result);
+        })
+
+        // chekcing the user if its is admin or not
+        app.get('/user/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'unauthorized access' })
+            }
+
+            const query = {email: email };
+            const user = await userCollection.findOne(query);
+            let admin = false;
+            if(user){
+                admin = user?.role === 'admin';
+            }
+            res.send({ admin })
         })
 
         // update users by admin
